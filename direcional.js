3d5578b9f89b4086b1cbdaeef6ba9a31c52cc076
Lucas Freire <97474252+Luc4sFreire@users.noreply.github.com>
2022-07-11 11:24:32 -0300

    atualização

---
+++ direcional.js
@@ -0,0 +1,12 @@
+const inicio = document.querySelector('#ainicio')
+const sobre = document.querySelector('#asobre')
+const projeto = document.querySelector('#aprojeto')
+const tec = document.querySelector('#atec')
+const btn = document.querySelector('#btn')
+
+inicio.addEventListener('clicar(), click')
+
+function clicar(){
+    const boxini = document.querySelector('#inicio')
+    boxini = 
+}
